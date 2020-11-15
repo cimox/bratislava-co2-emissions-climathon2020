16eb20ef3217fdef22ff182d7e1de5d526fbe3cf
Matus Cimerman <mcimerman@blueprintpower.com>
2020-11-15 13:42:46 +0100

    restructure run.py

---
+++ run.py
@@ -0,0 +1,23 @@
+import dash as dash
+
+from src.good.dashboard_builder import *
+
+
+app: dash.Dash = dash.Dash(
+    __name__, external_stylesheets=[
+        'https://codepen.io/chriddyp/pen/bWLwgP.css', './assets/stylesheets.css'
+    ],
+    suppress_callback_exceptions=True,
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+    ]
+)
+
+server = app.server
+    
+dashboard_builder = DashboardBuilder(app)
+dashboard_builder.build()
+
+
+if __name__ == '__main__':
+    pass
